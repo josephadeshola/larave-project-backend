15097541172bab67e7e9f00a97877e9a379040e6
Adeshola Ayomide <josephay125d@gmail.com>
2024-07-26 08:14:08 -0700

    logic for signun and login done

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Http\Controllers;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+use App\Models\User;
+use Illuminate\Http\Request;
+
+class AuthController extends Controller
+{
+    public function register(Request $request)
+    {
+        $user = User::create([
+            'name' => $request->input('name'),
+            'email' => $request->input('email'),
+            'city' => $request->input('city'),
+            'state' => $request->input('state'),
+            'password' => Hash::make($request->password),
+        ]);
+        return response()->json($user, 201);
+    }
+
+    public function login(Request $request){
+        $credentials = $request->only('email', 'password');
+        if (Auth::attempt($credentials)) {
+            $user = Auth::user();
+            $token = $user->createToken('authToken')->plainTextToken;
+
+            return response()->json([
+                'message' => 'Login successful',
+                'token' => $token
+            ], 200);
+        }
+
+        return response()->json(['message' => 'Invalid credentials'], 401);
+    }
+}
